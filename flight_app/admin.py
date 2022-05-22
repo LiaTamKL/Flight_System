6d7f2ec045f279c12f7710006783acda9bcf54c0
LiaTamKL <ellaor6@gmail.com>
2022-05-22 12:22:26 +0300

    Added DB values and an admin page

--- flight_app/admin.py
+++ flight_app/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ['Country_name', 'Flag']
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['Username', 'email', 'user_role']
+
+@admin.register(Airline)
+class AirlineAdmin(admin.ModelAdmin):
+    list_display = ['Name', 'User_id', 'Country_id']
+
+@admin.register(Flight)
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ['Airline_id', 'Origin_country_id', 'Destination_country_id', 'Departure_time', 'Landing_time', 'Remaining_tickets']
+
+@admin.register(User_Role)
+class UserRoleAdmin(admin.ModelAdmin):
+    list_display =['Role_name']
+
+
