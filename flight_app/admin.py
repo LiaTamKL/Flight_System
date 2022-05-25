cbb3fd7b6a868c0979a9526b70564fac6d1f7cbd
Kevans86 <kevevans1986@gmail.com>
2022-05-25 16:37:40 +0300

    roolback

--- flight_app/admin.py
+++ flight_app/admin.py
@@ -1,7 +1,10 @@
+
+from atexit import register
 from django.contrib import admin
 from .models import *
-
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+
 
 @admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
@@ -9,9 +12,6 @@ class CountryAdmin(admin.ModelAdmin):
     list_display_links = ('link',)
     list_editable = ['flag']
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'email', 'user_role']
 
 @admin.register(Airline)
 class AirlineAdmin(admin.ModelAdmin):
@@ -19,15 +19,81 @@ class AirlineAdmin(admin.ModelAdmin):
 
 @admin.register(Flight)
 class FlightAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request, obj=None):
+        return False
     list_display = ['airline', 'origin_country', 'destination_country', 'departure_time', 'landing_time', 'remaining_tickets']
 
-@admin.register(User_Role)
-class UserRoleAdmin(admin.ModelAdmin):
-    list_display =['role_name']
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display =['user' , 'first_name' , 'last_name']
+
+
+@admin.register(User_Role)
+class AccountRoleAdmin(admin.ModelAdmin):
+    list_display =['role_name']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####################################################
+        #old admin
+###################################################
+
+
+
+# from django.contrib import admin
+# from .models import *
+
+# # Register your models here.
+
+# @admin.register(Country)
+# class CountryAdmin(admin.ModelAdmin):
+#     list_display = ['country_name', 'flag', 'link']
+#     list_display_links = ('link',)
+#     list_editable = ['flag']
+
+# @admin.register(User)
+# class UserAdmin(admin.ModelAdmin):
+#     list_display = ['username', 'email', 'user_role']
+
+# @admin.register(Airline)
+# class AirlineAdmin(admin.ModelAdmin):
+#     list_display = ['name', 'user', 'country']
+
+# @admin.register(Flight)
+# class FlightAdmin(admin.ModelAdmin):
+#     list_display = ['airline', 'origin_country', 'destination_country', 'departure_time', 'landing_time', 'remaining_tickets']
+
+# @admin.register(User_Role)
+# class UserRoleAdmin(admin.ModelAdmin):
+#     list_display =['role_name']
+
+# @admin.register(Customer)
+# class CustomerAdmin(admin.ModelAdmin):
+#     list_display =['user' , 'first_name' , 'last_name']
 
 
 
