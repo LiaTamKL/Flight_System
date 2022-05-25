5003ee88e4667d3c50dd30f2618fa949ca582a09
LiaTamKL <ellaor6@gmail.com>
2022-05-25 14:01:22 +0300

    just fixing tests

--- flight_app/tests/test_flight_app.py
+++ flight_app/tests/test_flight_app.py
@@ -2,10 +2,15 @@ import pytest
 from rest_framework import status
 from rest_framework.test import APIClient
 
-class test_flight_app:
+print('hello im testing stuff')
+
+class Test_flight_app:
     def test_flight_app_home(self):
         client = APIClient()
-        print('hello there, lets test')
         response = client.get('/flight_app/')
         print(response.status_code)
         assert response.status_code == status.HTTP_200_OK
+
+
+    def test_this_will(self):
+        print('Win')
