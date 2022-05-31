5774c4fa8297c541045fb52ff506a3d20a86c348
LiaTamKL <ellaor6@gmail.com>
2022-05-31 12:51:09 +0300

    admin page

--- flight_app/views.py
+++ flight_app/views.py
@@ -60,7 +60,8 @@ def members_homepage(request):
             account = models.Airline.objects.get(account_id = account_id).name
             return view_flights_by_airline(request)
 
-
+        #elif account_type == models.Account_Role.objects.get(role_name = 'Admin'):
+        #    account = models.Administrator.objects.get(account_id = account_id).first_name
         context = {
 
             'account': account,
