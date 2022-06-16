30919121a88e1daf21407bad0b914b5d3c11d4a0
LiaTamKL <ellaor6@gmail.com>
2022-06-16 10:03:22 +0300

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,18 @@
 import React, { useContext } from 'react'
 import AuthContext from '../context/authentication'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   let {user} = useContext(AuthContext)
   return (
     <div className='app-header'>
         <h1>Flight list</h1>
+        <br/>
+        {user ? (
+                 <p>Logout</p>
+            ): (
+                <Link to="/login" >Login</Link>
+            )}
 
 
     </div>
