8cb130ccb3ef7fdc4e27acf0af96fd5464b976e1
LiaTamKL <ellaor6@gmail.com>
2022-06-21 17:34:58 +0300

    update account function's alpha is ready

--- backend/views.py
+++ backend/views.py
@@ -200,22 +200,56 @@ def user_api(request):
 
     else:
 
-        #This will be to get the user account and linked data for updates
+        #This gets the user account and linked data for updates
         if request.method=='GET':
             respon = get_user(request)
             return respon
 
         #This will be to update the data
         if request.method == 'PATCH':  
-            print(request.data)
-            return Response({'ERROR':'Not complete yet! Please dont use me!.'})
+            try:
+                account = Account.objects.exclude(pk=request.user).get(email=request.data['email'])
+                return Response(data='This email is already in use!', status=status.HTTP_400_BAD_REQUEST)
+            except Account.DoesNotExist:
+                form = {}
+                emailform={}
+                emailform['email'] = request.data['email']
+            if request.user.account_role == Account_Role.objects.get(role_name='Customer'):
+                try: 
+                    phonetest = Customer.objects.exclude(account=request.user).get(phone_number=request.data['phone_number'])
+                    return Response(data='Phone number already in use!', status=status.HTTP_400_BAD_REQUEST)
+                except Customer.DoesNotExist: 
+                    try:
+                        cardtest = Customer.objects.exclude(account=request.user).get(credit_card_no=request.data['credit_card_no'])
+                        return Response(data='Card number already in use!', status=status.HTTP_400_BAD_REQUEST)
+                    except Customer.DoesNotExist:
+                        form['credit_card_no'] = request.data['credit_card_no']
+                form['first_name'] = request.data['first_name']
+                form['last_name'] = request.data['last_name']
+                form['address'] = request.data['address']
+                form['phone_number'] = request.data['phone_number']
+                AdministratorFuncade.update_customer(account=request.user, form=form, emailform=emailform)
 
-        #This will be to delete a user. Only admins may use it.
+            elif request.user.account_role == Account_Role.objects.get(role_name='Airline'):
+                country = Country.objects.get(country_name=request.data['country'])
+                form['country'] =  country
+                form['name'] = request.data['name']
+                Airline_Facade.update_airline(account=request.user, form=form, emailform=emailform)
+            elif request.user.is_superuser == True:
+                AdministratorFuncade.update_account(account=request.user, form=emailform)
+            elif request.user.is_admin == True:
+                form['first_name'] = request.data['first_name']
+                form['last_name'] = request.data['last_name']
+                AdministratorFuncade.update_admin(account=request.user, form=form.cleaned_data, emailform=emailform.cleaned_data)
+            else: 
+                return Response(data='This account seems to not be any user type. Please contact an admin.', status=status.HTTP_400_BAD_REQUEST)
+
+            return Response(f'Account for {request.user.account_role} {request.user.username} updated successfully')
+
+        #This will be to delete current user. 
         if request.method == 'DELETE':  
-            if not request.user.is_admin:
-                return response(data="Only administrators may take this action", status=status.HTTP_401_UNAUTHORIZED)
             print(request.data)
-            return Response({'ERROR':'Not complete yet! Please dont use me!.'})
+            return Response('Extra feature! it may not be in use by the final product but if we want to, its here for us to implement.')
 
 ##################### ADMIN ACTIONS ############################
 
@@ -227,7 +261,7 @@ def admin_api(request):
     if request.user.is_admin == False:
          return Response(data='Must be admin to use!', status=status.HTTP_401_UNAUTHORIZED)
     
-    #this expects you to enter a view (Airlines, Admins, Customers, Specific)
+    #this expects you to enter a view value (Airlines, Admins, Customers, Specific)
     #it will either return all accounts of that type
     #if you use specific, state a username and it will return it
     if request.method == 'POST':
