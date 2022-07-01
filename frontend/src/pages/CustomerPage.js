54c2fa456e1ff51e5af25023ffa6b3fb222eed54
Kevans86 <kevevans1986@gmail.com>
2022-07-01 14:04:16 +0300

    updates

--- frontend/src/pages/CustomerPage.js
+++ frontend/src/pages/CustomerPage.js
@@ -1,18 +1,28 @@
 
-import React , {useContext} from 'react';
+import React , {useContext , useEffect , useState} from 'react';
 import TicketFrom from '../forms/TicketFrom';
 import AuthContext from "../context/authentication";
 import { Link, useNavigate } from "react-router-dom";
 
 const CustomerPage = () => {
+    let [tickes, setTickets] = useState();
+
+
+    const getTickets = async () => {
+
+    }
+
+    
     // let {user} = useContext(AuthContext)
     // user.account_role ==='Customer'? 
     // console.log("yes"): 
     // console.log("no");
   
+
+
     return (
     <div>
-
+    
     </div>
   )
   
