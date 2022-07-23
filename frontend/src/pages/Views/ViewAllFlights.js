2d463670b7331d8ece7650c6fd0039e0c4994684
Kevans86 <kevevans1986@gmail.com>
2022-07-23 19:38:28 +0300

    Update ViewAllFlights.js

--- frontend/src/pages/Views/ViewAllFlights.js
+++ frontend/src/pages/Views/ViewAllFlights.js
@@ -9,6 +9,8 @@ import Select from 'react-select'
 import NewFlightForm from "../../forms/NewFlightForm";
 import ReactPaginate from "react-paginate"
 import { AllCountries } from "../../methods/CountriesMethods";
+import FormTemplate from '../../forms/FormTemplate/FormTemplate';
+
 
 const ViewAirlineFlights= () => {
     const [searched, setSearched] = useState(false);
@@ -86,6 +88,8 @@ const ViewAirlineFlights= () => {
              setMessage(is_form_valid)
          }
          else{
+
+
              let result = await UpdateMyFlight(e,update.id, authToken)
              setMessage(result.data)
              setBack()
@@ -94,7 +98,7 @@ const ViewAirlineFlights= () => {
     }
 
     const [pagenumber, setPageNumber] = useState(0)
-    const flightsPerPage = 6
+    const flightsPerPage = 3
     const pagesSeen = pagenumber * flightsPerPage
 
 
@@ -103,14 +107,14 @@ const ViewAirlineFlights= () => {
     */
     const displayFlights = flights.slice(pagesSeen, pagesSeen + flightsPerPage).map((flight)=>{
     return (
-            <FlightCard key={flight.id} className flight={flight} countries={countries} custPage = {false} updateDeleteBtn = {
+        <>
+            <FlightCard className flight={flight} countries={countries} custPage = {false} updateDeleteBtn = {
             <div className="btn-group">
                 <button onClick={()=>Delete(flight.id)} className="btn btn-danger btn-sm" >Delete</button>
                 <button onClick={()=>setUpdate(flight)} className="btn btn-primary btn-sm" >Update</button>
-            </div>
-                
+            </div> 
             } />
-
+    </>
     )})
     const pageCount = Math.ceil(flights.length / flightsPerPage)
     const changePage = ({selected})=>{
@@ -124,12 +128,19 @@ const ViewAirlineFlights= () => {
 
         </div>
 
-        {message? (<p className="alert alert-secondary">{message}</p>):<></>}
+        {/* {message? (<p className="alert alert-secondary">{message}</p>):<></>} */}
         {update? (
-        
+        // ####################################################################
         <form onSubmit={(e)=>handleUpdate(e)}>
-        <NewFlightForm flightData={update}/>
-        <input type="submit" className="btn btn-primary btn-sm" value={'Update Flight #'+update.id}/>
+
+            <FormTemplate 
+                formName= {`Update Flight  #${ update.id }`}
+                btnDesc = { 'Update' }
+                formFields = { <NewFlightForm flightData={update}/>}
+                formErrors = {message? (<p className="alert alert-warning">{message}</p>):<></>}
+            />
+        {/* <NewFlightForm flightData={update}/> */}
+        {/* <input type="submit" className="btn btn-primary btn-sm" value={'Update Flight #'+update.id}/> */}
         </form>
         
         )
@@ -153,21 +164,23 @@ const ViewAirlineFlights= () => {
 
 
         {searched?<>
-        {<>
-        <h2 className='flights-title'>Searched for:</h2>
+        {
+        <>
+        {/* <div key={searched.id} className="list-group-item list-group-item-action flex-column align-items-start"> */}
+        <div id='searched-for-container'><label id='searched-for-label' >Searched for:</label> </div>
         {/* <FlightCard flight={searched} countries={countries} custPage = {false}/>
         <button onClick={()=>Delete(searched.id)}className="btn btn-danger btn-sm" >Delete</button>
         <button onClick={()=>setUpdate(searched)}className="btn btn-primary btn-sm" >Update</button> */}
-        <FlightCard className flight={searched} countries={countries} custPage = {false} updateDeleteBtn = {
-            <div className="btn-group">
-                <button onClick={()=>Delete(searched.id)} className="btn btn-danger btn-sm" >Delete</button>
-                <button onClick={()=>setUpdate(searched)} className="btn btn-primary btn-sm" >Update</button>
-            </div>
-                
-            } />
-
-
-</>
+        <div id="center-flightcard">
+            <FlightCard  flight={searched} countries={countries} custPage = {false} updateDeleteBtn = {
+                <div className="btn-group">
+                    <button onClick={()=>Delete(searched.id)} className="btn btn-danger btn-sm" >Delete</button>
+                    <button onClick={()=>setUpdate(searched)} className="btn btn-primary btn-sm" >Update</button>
+                </div> 
+                } />
+         </div>  
+         {/* </div> */}
+        </>
         }</>
         :<>
         
