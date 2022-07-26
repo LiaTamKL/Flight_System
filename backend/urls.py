4ac6ed4bdcfa9c8b62a38269f4b0d22f2ad99a75
LiaTamKL <ellaor6@gmail.com>
2022-07-26 15:46:33 +0300

    fixing get country...

--- backend/urls.py
+++ backend/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
 
     path('api/customer_api', tickets_api),
 
-    path ('countries/', country_api),
+    path ('countries', country_api),
     path ('countries/<str:id>/', specific_country_api),
 
 
