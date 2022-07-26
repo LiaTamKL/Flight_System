d44d41d8d685180f2b1792e9ad714ce841fb9993
LiaTamKL <ellaor6@gmail.com>
2022-07-26 11:48:17 +0300

    fixes

--- Flight_System/asgi.py
+++ Flight_System/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Flight_System.settings.dev_set')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Flight_System.settings.prod_set')
 
 application = get_asgi_application()
--- Flight_System/wsgi.py
+++ Flight_System/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Flight_System.settings.dev_set')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Flight_System.settings.prod_set')
 
 application = get_wsgi_application()
