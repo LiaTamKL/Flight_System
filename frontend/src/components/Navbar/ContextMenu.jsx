9ec68af6b1c316320b5b25805a9e91595c50f981
LiaTamKL <ellaor6@gmail.com>
2022-08-14 09:40:21 +0300

    admin to customer fix

--- frontend/src/components/Navbar/ContextMenu.jsx
+++ frontend/src/components/Navbar/ContextMenu.jsx
@@ -55,7 +55,6 @@ const UserLinks = () => {
 
                 <Nav.Link href="/admin/view_admins">View All Admins</Nav.Link>
                 <Nav.Link href="/admin/view_airlines">View All Airlines</Nav.Link>
-                <Nav.Link href="/admin/make_country">Add A Country</Nav.Link>
                 <Nav.Link href="/admin/view_specific">Search For Users</Nav.Link>
                 <UserLinks />               
             </>
