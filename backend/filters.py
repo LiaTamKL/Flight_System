fe09d0a68cf92fef474432d95b3c675a9e0f17ef
LiaTamKL <ellaor6@gmail.com>
2022-08-14 14:53:09 +0300

    Update filters.py

--- backend/filters.py
+++ backend/filters.py
@@ -6,8 +6,7 @@ from .models import *
 class Flightfilter(FilterSet):
     from_departure_time = IsoDateTimeFilter(field_name='departure_time',lookup_expr='gte')
     to_arrival_time = IsoDateTimeFilter(field_name='landing_time', lookup_expr='lte')
-    # positive_tickets_number = NumberFilter(field_name='remaining_tickets', lookup_expr='gt') 
-    
+     
     class Meta:
         model = Flight
         fields = {
@@ -21,13 +20,6 @@ class Flightfilter(FilterSet):
 
         }
 
-# class Ticketfilter(FilterSet):
-#     class Meta:
-#         model = Flight_Ticket
-#         fields = {
-#             'customer': ["in", "exact"],
-#             'flight': ["in", "exact"]
-#         }
 
 class Countryfilter(FilterSet):
     class Meta:
